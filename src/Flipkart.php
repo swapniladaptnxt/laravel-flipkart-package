41a242e7ddc4374ca1bf0b3fe64b951137011751
swapnil yadav <swapnil@adaptnxt.com>
2021-10-06 12:45:35 +0530

    add & update stock api

--- src/Flipkart.php
+++ src/Flipkart.php
@@ -52,10 +52,65 @@ class Flipkart {
 
     public function filter(string $status = 'all', string $source = 'all', int $page = 1){
         return $this->call("/shipments/filter", [
+            
             'source' => $source,
             'status' => $status,
             'page' => $page
         ]);
     }
+    // order
+    public function getOrderDetails($orderItemIds){
+        try {
+                if($orderItemIds){
+                    $result=$this->_API->call(['URL'=>'/sellers/v2/orders/shipments?orderItemIds='.$orderItemIds, 'METHOD' => "GET",'RETURNARRAY'=>true]);
+                    return $result;
+                }
+        } catch (Exception $e) {
+            
+        }
+    }
+
+    // stock
+    public function updateInventory($data){
+        try
+        {
+            $result=array();
+            if($data){
+                $result=$this->_API->call(['URL'=>'/sellers/listings/v3/update/inventory', 'METHOD' => "POST",'DATA'=>$data,'ALLDATA'=>true,'RETURNARRAY'=>true]);
+            }
+        }
+        catch (Exception $e)
+        {
+            $result = $e->getMessage();
+        }
+
+        return $result;
+    }
+
+    // update stock
+
+    public function updateApiInventory($sku=NULL,$location_id,$qty){
+        try
+        {
+            $result=array();
+            if($sku){
+                $detail=$this->getSkudetail($sku);
+                $locations=[['id'=>$location_id,'inventory'=>$qty]];
+                $data=array();
+                foreach ($detail as $key => $value) {
+                    $data[$key]=['product_id'=>$value['product_id'],'locations'=>$locations];
+                }
+                $result=$this->_API->call(['URL'=>'/sellers/listings/v3/update/inventory', 'METHOD' => "POST",'DATA'=>$data,'ALLDATA'=>true,'RETURNARRAY'=>true]);
+            }
+        }
+        catch (Exception $e)
+        {
+            $result = $e->getMessage();
+        }
+
+        return $result;
+    }
+
+
 
 }
